36896a8330aec736f99a2a567c78cdd9c2efffff
André <d3roch4@gmail.com>
2014-10-06 14:58:04 -0300

    add files

---
+++ WebContent/js/materiais.js
@@ -0,0 +1,31 @@
+﻿var Material = function()
+{
+	this.nome="";
+	this.descricao="";
+	this.lote="";
+	this.validade="";
+	this.imagem="";
+}
+
+function mostrarFormNovoMaterial()
+{
+	$(".formNovoMaterial").toggle();
+}
+function getTodosMateriais()
+{
+	var list = new Array;	
+	var mat = new Material();
+	mat.nome = "Acido";
+	mat.lote="9s8fjv";
+	list.push(mat);
+	mat = new Material();
+	mat.nome="açucar";
+	mat.validade="01/05/2013";
+	list.push(mat);
+	return list;
+}
+function mostrarTodosMateriais()
+{
+	var listMateriais = getTodosMateriais();
+	alert(listMateriais);	
+}
